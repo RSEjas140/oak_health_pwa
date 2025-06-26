1edb9c2a5348e5db5e707998ad850896cd223139
james <jas140@aber.ac.uk>
2025-06-26 10:12:48 +0100

    remove questions 1 and 2

--- questions.js
+++ questions.js
@@ -4,33 +4,14 @@
 //order is important that it is same as questions and then final meta data order is (uid,ts,long,lat)
 // custom question skipping based on user answers to questions. If question order modified question skip params need to be updated
 
-export const headers = ['compartment_id','subcompartment_id', 'tree_id', 'species', 'social_class', 'crown_obs', 'crown_transpar', 'cross_miss',  'live_crown',
+export const headers = ['tree_id', 'species', 'social_class', 'crown_obs', 'crown_transpar', 'cross_miss',  'live_crown',
 'tree_height', 'dbh', 'active_bleeds', 'bleed_length', 'staining', 'stain_length', 'highest_stain_bleed', 'agrilus_emergence_holes', 'defoli', 'fungal_growth_tree',  'fungal_growth_roots', 'mildew',
  'photos_taken', 'uid','ts','longitude', 'latitude']
 
 export const questions = [
+    
     {
         "id": 0,
-        "label": "Compartment ID",
-        "type": "select",
-        "options": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        "required": true,
-        "image": null,
-        "info": null,
-        "note": null
-    },
-    {
-        "id": 1,
-        "label": "Sub-compartment ID",
-        "type": "select",
-        "options": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-        "required": true,
-        "image": null,
-        "info": null,
-        "note": null
-    },
-    {
-        "id": 2,
         "label": "Tree Identifier",
         "type": "text",
         "required": true,
@@ -39,7 +20,7 @@ export const questions = [
         "note": null
     },
     {
-        "id": 3,
+        "id": 1,
         "label": "Species",
         "type": "select",
         "options": ["Quercus robur", "Quercus petraea", "Hybrid", "Unknown Native Oak"],
@@ -49,7 +30,7 @@ export const questions = [
         "note": null
     },
     {
-        "id": 4,
+        "id": 2,
         "label": "Social Class",
         "type": "radio",
         "options": ["1. Dominant (No neighbour is taller)", "2. Co-dominant (Neighbours are the same height)", "3. Sub-dominant (Neighbours taller but not shaded)", "4 - Suppressed (Shaded out by neighbours)"],
@@ -60,7 +41,7 @@ export const questions = [
     },
 
     {
-        "id": 5,
+        "id": 3,
         "label": "How much of tree crown is missing ?",
         "type": "range",
         "rmin": 5,
@@ -73,7 +54,7 @@ export const questions = [
     },
 
     {
-        "id": 6,
+        "id": 4,
         "label": "Where are you standing to collect your data?",
         "type": "radio",
         "options": ["Under Tree", "Away from Tree"],
@@ -83,7 +64,7 @@ export const questions = [
         "note": "Your position is important for the next question on crown transparency. Please stay in place until it’s completed.",
     },
     {
-        "id": 7,
+        "id": 5,
         "label": "How much of the tree crown is transparent ?",
         "type": "range",
         "rmin": 5,
@@ -97,7 +78,7 @@ export const questions = [
     
 
     {
-        "id": 8,
+        "id": 6,
         "label": "Are there signs of insect defoliation?",
         "type": "radio",
         "options": ["Yes", "No"],
@@ -108,7 +89,7 @@ export const questions = [
     },
     
     {
-        "id": 9,
+        "id": 7,
         "label": "What is the height of the lowest living crown?",
         "type": "number",
         "min": 1,
@@ -119,7 +100,7 @@ export const questions = [
         "note": "Please provide your answer in metres"
     },
     {
-        "id": 10,
+        "id": 8,
         "label": "What is the height of the tree?",
         "type": "number",
         "min": 1,
@@ -130,7 +111,7 @@ export const questions = [
         "note": null
     },
     {
-        "id": 11,
+        "id": 9,
         "label": "What is the circumference(cm) at 1.3m from the ground? ",
         "type": "number",
         "min": 50,
@@ -141,7 +122,7 @@ export const questions = [
         "note": null
     },
     {
-        "id": 12,
+        "id": 10,
         "label": "What is the average number of active bleeds (liquid currently running out of bark crack) ?",
         "type": "number",
         "min": 0,
@@ -152,7 +133,7 @@ export const questions = [
         "note": null
     },
     {
-        "id": 13,
+        "id": 11,
         "label": "What is the average length of the bleeds (in centimetres) ?",
         "type": "number",
         "min": 1,
@@ -161,11 +142,11 @@ export const questions = [
         "image": "graphics/activebleed.jpg",
         "info": "Measure from the top of the crack to the lowest point at which the dark substance has bled.",
         "note": null,
-        "showIf": (answers) => parseFloat(answers[12]) > 0
+        "showIf": (answers) => parseFloat(answers[10]) > 0
     },
     
     {
-        "id": 14,
+        "id": 12,
         "label": "What is the average number of dry black stains?",
         "type": "number",
         "min": 0,
@@ -176,7 +157,7 @@ export const questions = [
         "note": ""
     },
     {
-        "id": 15,
+        "id": 13,
         "label": "What is the average length of the stains in cm?",
         "type": "number",
         "min": 1,
@@ -185,10 +166,10 @@ export const questions = [
         "image": "graphics/blackstain.JPG",
         "info": "Measure from the top of the crack to the lowest point at which the bark is stained black.",
         "note": null,
-        "showIf": (answers) => parseFloat(answers[14]) > 0
+        "showIf": (answers) => parseFloat(answers[12]) > 0
     },
     {
-        "id": 16,
+        "id": 14,
         "label": "What is the height of the highest bleed or stain (in metres) ?",
         "type": "number",
         "min": 1,
@@ -197,10 +178,10 @@ export const questions = [
         "image": "graphics/activebleed.jpg",
         "info": null,
         "note": null,
-        "showIf": (answers) => parseFloat(answers[12]) > 0 || parseFloat(answers[14]) > 0
+        "showIf": (answers) => parseFloat(answers[10]) > 0 || parseFloat(answers[12]) > 0
     },
     {
-        "id": 17,
+        "id": 15,
         "label": "Are there D-shaped Agrilus emergence holes?",
         "type": "radio",
         "options": ["Yes", "No"],
@@ -212,7 +193,7 @@ export const questions = [
 
     
     {
-        "id": 18,
+        "id": 16,
         "label": "Is there evidence of fungal growth?",
         "type": "radio",
         "options": ["Yes", "No"],
@@ -222,7 +203,7 @@ export const questions = [
         "note": "Assess the whole tree. Can you see fungal fruiting bodies emerging from the bark?"
     },
     {
-        "id": 19,
+        "id": 17,
         "label": "Are there signs of fungal fruiting bodies near the roots?",
         "type": "radio",
         "options": ["Yes", "No"],
@@ -232,7 +213,7 @@ export const questions = [
         "note": null,
     },
     {
-        "id": 20,
+        "id": 18,
         "label": "Are there signs of mildew?",
         "type": "radio",
         "options": ["Yes", "No"],
@@ -243,7 +224,7 @@ export const questions = [
     },
     
     {
-        "id": 21,
+        "id": 19,
         "label": "Please take supporting photos of any stem symptoms and indicate if you have here.",
         "type": "radio",
         "options": ["Yes", "No"],
